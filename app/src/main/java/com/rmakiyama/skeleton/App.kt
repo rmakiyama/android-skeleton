42a816140fb9ed5dfd3486bbc999b51594335a88
rmakiyama <rmakiyama.io@gmail.com>
2020-04-01 23:48:19 +0900

    set material theme

--- app/src/main/java/com/rmakiyama/skeleton/App.kt
+++ app/src/main/java/com/rmakiyama/skeleton/App.kt
@@ -1,5 +1,8 @@
 package com.rmakiyama.skeleton
 
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.Q
+import androidx.appcompat.app.AppCompatDelegate
 import com.rmakiyama.skeleton.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -10,6 +13,12 @@ class App : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        val nightMode = if (SDK_INT >= Q) {
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        } else {
+            AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+        }
+        AppCompatDelegate.setDefaultNightMode(nightMode)
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
